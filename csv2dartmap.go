f13a53295cf8e604806f998b84fc95eb2a90125b
Kim, Jong-kook <kjk15881588@gmail.com>
2020-12-30 13:03:13 +0900

    add dart file

--- csv2dartmap.go
+++ csv2dartmap.go
@@ -2,45 +2,45 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/csv"
 	"fmt"
-	"os"
 	"log"
-	"bytes"
+	"os"
 	"time"
 )
 
 func generateDartMapString(records [][]string) string {
 	var buf bytes.Buffer
 	buf.WriteString("// generated " + time.Now().String() + "\n")
-	buf.WriteString("Map<String, Map<String, String>> localized_values = {\n")
+	buf.WriteString("Map<String, Map<String, String>> localizedValues = {\n")
 
 	if len(records) > 0 {
-		
+
 		header := records[:1]
 		data := records[1:]
-		
+
 		for headerIdx, lang := range header[0][1:] {
 			buf.WriteString("\t'")
 			buf.WriteString(lang)
 			buf.WriteString("': {\n")
-			
+
 			for _, row := range data {
 				buf.WriteString("\t\t'")
 				buf.WriteString(row[0])
 				buf.WriteString("': '")
 				buf.WriteString(row[headerIdx+1])
-				buf.WriteString("',\n")				
+				buf.WriteString("',\n")
 			}
 			buf.WriteString("\t},\n")
-		} 		
+		}
 	}
-		
+
 	buf.WriteString("};")
 	return buf.String()
 }
 
-func csv2dartmap(srcName string, destName string)(err error) {
+func csv2dartmap(srcName string, destName string) (err error) {
 	file, err := os.Open(srcName)
 	if err != nil {
 		return
@@ -60,15 +60,15 @@ func csv2dartmap(srcName string, destName string)(err error) {
 	if err != nil {
 		return
 	}
-	
+
 	dartString := generateDartMapString(records)
-	_, err = fmt.Fprintf(file, dartString)	
+	_, err = fmt.Fprintf(file, dartString)
 	return
 }
 
 func main() {
 	// File open
-	err := csv2dartmap("./localized_values.csv", "./generated.dart")
+	err := csv2dartmap("./localized_values.csv", "./localized_values.dart")
 	if err != nil {
 		log.Fatal(err)
 	}
